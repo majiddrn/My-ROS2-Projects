c70a6003afdfdb7c9d6bc213c7737ab9f59e180a
majiddrn <dareinimajid34@gmail.com>
2023-05-26 11:55:02 +0330

    eddiebot_controller_color added

---
+++ src/eddiebot_controller_color/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'eddiebot_controller_color'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='majiddrn',
+    maintainer_email='dareinimajid34@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
